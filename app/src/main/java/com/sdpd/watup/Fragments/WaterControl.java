9f4575d5cfad75f2ace6d64931dc6d959865004a
2015B3A70477G <f20150477@goa.bits-pilani.ac.in>
2019-03-06 21:55:19 +0530

    First commit

--- app/src/main/java/com/sdpd/watup/Fragments/WaterControl.java
+++ app/src/main/java/com/sdpd/watup/Fragments/WaterControl.java
@@ -7,11 +7,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sdpd.watup.R;
 
 public class WaterControl extends Fragment {
 
+    private SeekBar seekBar;
+    private TextView textView;
 
     public WaterControl() {
         // Required empty public constructor
@@ -30,6 +36,33 @@ public class WaterControl extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_water_control, container, false);
 
+        seekBar = (SeekBar) view.findViewById(R.id.seekBar1);
+        textView = (TextView) view.findViewById(R.id.textView1);
+
+        textView.setText("Covered: " + seekBar.getProgress() + "/" + seekBar.getMax());
+
+        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+                Toast.makeText(getActivity().getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getActivity().getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textView.setText("Covered: " + progress + "/" + seekBar.getMax());
+                Toast.makeText(getActivity().getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return view;
     }
+
 }
--- app/src/main/java/com/sdpd/watup/Fragments/WaterLevelGraph.java
+++ app/src/main/java/com/sdpd/watup/Fragments/WaterLevelGraph.java
@@ -1,13 +1,15 @@
 package com.sdpd.watup.Fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.jjoe64.graphview.GraphView.GraphViewData;
+import com.jjoe64.graphview.GraphViewSeries;
+import com.jjoe64.graphview.LineGraphView;
 import com.sdpd.watup.R;
 
 
@@ -27,8 +29,32 @@ public class WaterLevelGraph extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_water_level_graph, container, false);
-
+        populateGraphView(view);
 
         return view;
+    }
+
+    private void populateGraphView(View view) {
+        GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {
+                new GraphViewData(1, 2.0d)
+                , new GraphViewData(2, 1.5d)
+                , new GraphViewData(3, 2.5d)
+                , new GraphViewData(4, 1.0d)
+        });
+
+        LineGraphView graphView = new LineGraphView(
+                getActivity() // context
+                , "Water Consumption Chart" // heading
+        );
+
+        graphView.addSeries(exampleSeries); // data
+
+        try {
+            LinearLayout layout;
+            layout = view.findViewById(R.id.graph1);
+            layout.addView(graphView);
+        }catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 }
