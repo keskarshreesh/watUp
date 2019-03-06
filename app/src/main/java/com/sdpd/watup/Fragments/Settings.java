f93b58b2eca3cf8ba8bafb93ba81503c3da85c83
G-LOKi <garg.lokesh96@gmail.com>
2019-03-06 22:55:37 +0530

    final Changes

--- app/src/main/java/com/sdpd/watup/Fragments/Settings.java
+++ app/src/main/java/com/sdpd/watup/Fragments/Settings.java
@@ -28,27 +28,7 @@ public class Settings extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_settings, container, false);
-        onAutomaticButtonClicked(view);
-        
+
         return view;
-    }
-
-    public void onAutomaticButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch (view.getId()) {
-            case R.id.auto_switch:
-                if (checked)
-                    // Pirates are the best
-                    Toast.makeText(getActivity().getApplicationContext(), "Main Switch Control Automated", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.notifications:
-                if (checked)
-                    // Ninjas rule
-                    Toast.makeText(getActivity().getApplicationContext(), "Notifications on", Toast.LENGTH_SHORT).show();
-                break;
-        }
     }
 }
